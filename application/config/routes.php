3ff6ff9567bcc883270e9d252460fa5e67432529
Trisna Nugraha <trisna.nugraha123@gmail.com>
2021-12-10 08:36:48 +0700

    added custom routes for login page

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,12 +1,11 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 $route['default_controller'] = 'login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$route['panel_admin'] = 'Login_admin/index';
+$route['panel-admin'] = 'Login_admin/index';
 $route['jadwal-registrasi'] = 'Jadwal_registrasi/index';
 $route['tahun-angkatan'] = 'Tahun_angkatan/index';
 $route['program-studi'] = 'Program_studi/index';
-
